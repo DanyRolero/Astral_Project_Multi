1b573c32aa79b365a4b201839c54182caafda1c5
DanyRolero <dany_rolero@hotmail.com>
2022-11-01 18:40:08 +0100

    Sincro-01-11-22

--- World_Gen/_GridGraphBuilders/OddEvenConnector.cs
+++ World_Gen/_GridGraphBuilders/OddEvenConnector.cs
@@ -1,6 +1,37 @@
 ﻿using System;
 
-public class OddEvenConnector
+public class OddEvenConnector : GridGraphBuilder
 {
-    
+    Grid<int> adjacentsMask;
+    public OddEvenConnector(Grid<int> adjacentsMask)
+    {                 
+        this.adjacentsMask = adjacentsMask;
+    }
+
+    public override void Build(GridGraph graph)
+    {
+        for (int i = 0; i < graph.length; i++)
+        {
+            if (adjacentsMask[i] != -1)
+            {
+                int y = i / graph.columns;
+                int x = i % graph.columns;
+
+                if (y > 0) graph.AddAdjacent(i, i - graph.columns);
+                if (x > 0) graph.AddAdjacent(i, i - 1);
+                if (x < graph.columns - 1) graph.AddAdjacent(i, i + 1);
+                if (y < graph.rows - 1) graph.AddAdjacent(i, i + graph.columns);
+
+
+            }
+        }
+    }
+
+    public GridGraph Create(Grid<int> mask)
+    {
+        GridGraph graph = new GridGraph(mask.columns, mask.rows);
+
+
+        return graph;
+    } 
 }
