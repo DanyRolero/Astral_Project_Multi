cb152b79ac7e34918c7b2d388ba15bf18b0132a7
Danito <35589467+DanyRolero@users.noreply.github.com>
2022-11-11 11:43:53 +0100

    Sincro

--- World_Gen/_GridGraphBuilders/MazesBuilders/BinaryTree.cs
+++ World_Gen/_GridGraphBuilders/MazesBuilders/BinaryTree.cs
@@ -1,61 +1,47 @@
 ﻿public class BinaryTree : GridGraphBuilder
 {
-    public float coinRatio
-    {
-        get { return coinRatio; }
-        set
-        {
-            if (value < 0) coinRatio = 0;
-            else if (value > 1) coinRatio = 1;
-            else coinRatio = value;
-        }
-    }
+    float coinRatio;
+    Corner origin;
 
-    public Corner origin;
+    GridGraph graph;
+    Direction horizontalDirection;
+    Direction verticalDirection;
+
 
     public BinaryTree(float coinRatio = 0.5f, Corner origin = Corner.Left_Down)
     {
         this.coinRatio = coinRatio;
         this.origin = origin;
-    }
-
-
-    private bool FlipCoin()
-    {
-        Random random = new Random();
-        return random.NextSingle() > coinRatio;    
+        SetDirections();
     }
 
     public override void Build(GridGraph graph)
     {
-        for (int i = 0; i < graph.length; i++)
-        {
-            int y = i / graph.columns;
-            int x = i % graph.columns;
-            bool result = FlipCoin();
-
-            if (origin == Corner.Left_Down)
-            {
-                graph.AddAdjacent(i, i - graph.columns);
-            }
-
-            else if(origin == Corner.Left_Up)
-            {
-
-            }
-
-            else if(origin == Corner.Right_Up)
-            {
-
-            }
-
-            else if (origin == Corner.Right_Up)
-            {
-
-            }
-
-            
-        }
+        this.graph = graph;
+        
     }
 
+    private void SetDirections()
+    {
+        if(origin == Corner.Right_Up)
+        {
+            horizontalDirection = Direction.Left;
+            verticalDirection = Direction.Down;
+        }
+        else if (origin == Corner.Right_Down)
+        {
+            horizontalDirection = Direction.Left;
+            verticalDirection = Direction.Up;
+        }
+        else if (origin == Corner.Left_Down)
+        {
+            horizontalDirection = Direction.Right;
+            verticalDirection = Direction.Up;
+        }
+        else if (origin == Corner.Left_Up)
+        {
+            horizontalDirection = Direction.Right;
+            verticalDirection = Direction.Down;
+        }
+    }
 }
---
+++ World_Gen/_Random/AstralRandom.cs
@@ -0,0 +1,10 @@
+﻿public static class AstralRandom
+{
+    public static bool FlipCoin(float ratio = 0.5f)
+    {
+        Random random = new Random();
+        return random.NextSingle() > ratio;
+    }
+}
+
+
